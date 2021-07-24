71f5461df3fcbf29fcf7b07d7dbecc2a9467bdcb
Nelli Butenko <butenkonelly93@gmail.com>
2021-07-24 18:27:42 -0400

    Add useRouteMatch to keep the url dynamic

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, useRouteMatch } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 import EditProfileForm from "./EditProfileForm";
 
@@ -8,15 +8,17 @@ export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
 
-  // call useRouteMatch() to get the url and path
+  let { url, path } = useRouteMatch();
   
   // use loggedIn to return a Redirect
   
   return (
     <main>
       <h1>{currentUser.username}</h1>
-      <Link to={"/profile/edit"}>Edit</Link>
-      {/* Render a route for EditProfileForm */}
+      <Link to={`${path}/edit`}>Edit</Link>
+      <Route path={`${url}/edit`}>
+        <EditProfileForm />
+      </Route>
     </main>
   )
 }
