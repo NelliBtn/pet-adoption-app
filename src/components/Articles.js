1d978bae134c328c533219f85407a7183904024a
Nelli Butenko <butenkonelly93@gmail.com>
2021-07-25 11:48:53 -0400

    useLocation to extract the query parameter and use it for article search

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,16 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Articles () {
   const articles = useSelector(selectArticles)
   
-  // grab the search value from useLocation()
-  
-  // get the queryParams from new URLSearchParams() 
-  
-  const title = '' // <-- fix me!
+  const { search } = useLocation(); // get current URL's query string
+  const queryParams = new URLSearchParams(search); // get queryParams value (that has methods to get the query param)
+  const title = queryParams.get('title'); // use .get method and the name of the query parameter to extract the value
   
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
